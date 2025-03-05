54c7667d5e34764df857c581687b7a67498502cd
Jumpei Togashi <42961651+jtogashi@users.noreply.github.com>
2025-03-05 19:31:32 +0900

    Isochrone: Set camera and annotation to University of Tokyo

--- app/src/main/java/cc/jtogashi/mapboxmapsupporthelper/DefinedLocation.kt
+++ app/src/main/java/cc/jtogashi/mapboxmapsupporthelper/DefinedLocation.kt
@@ -4,4 +4,5 @@ import com.mapbox.geojson.Point
 
 object DefinedLocation {
     val TOKYO_TOWER: Point = Point.fromLngLat(139.7455, 35.6585)
+    val UNIVERSITY_OF_TOKYO: Point = Point.fromLngLat(139.75939, 35.71291)
 }
--- app/src/main/java/cc/jtogashi/mapboxmapsupporthelper/IsochroneActivity.kt
+++ app/src/main/java/cc/jtogashi/mapboxmapsupporthelper/IsochroneActivity.kt
@@ -3,18 +3,37 @@ package cc.jtogashi.mapboxmapsupporthelper
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import cc.jtogashi.mapboxmapsupporthelper.databinding.ActivityIsochroneBinding
+import com.mapbox.geojson.Point
+import com.mapbox.maps.dsl.cameraOptions
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
 
 class IsochroneActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIsochroneBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val binding = ActivityIsochroneBinding.inflate(layoutInflater).apply {
+        binding = ActivityIsochroneBinding.inflate(layoutInflater).apply {
+            mapIsochrone.mapboxMap.apply {
+                setCamera(
+                    cameraOptions {
+                        center(DefinedLocation.UNIVERSITY_OF_TOKYO)
+                        zoom(15.0)
+                    }
+                )
+            }
         }
+
+        addAnnotation(DefinedLocation.UNIVERSITY_OF_TOKYO)
 
         setContentView(binding.root)
 
@@ -23,5 +42,17 @@ class IsochroneActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun addAnnotation(point: Point) {
+        val image = ContextCompat.getDrawable(this, R.drawable.marker)?.toBitmap() ?: return
+        val annotationPlugin = binding.mapIsochrone.annotations
+        val manager = annotationPlugin.createPointAnnotationManager()
+        manager.create(
+            PointAnnotationOptions()
+                .withPoint(point)
+                .withIconImage(image)
+                .withIconSize(2.5)
+        )
     }
 }
