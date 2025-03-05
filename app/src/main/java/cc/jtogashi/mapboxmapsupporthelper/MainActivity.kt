4d9cebe2972654055ee91444c5529253b357a937
Jumpei Togashi <42961651+jtogashi@users.noreply.github.com>
2025-03-05 17:10:11 +0900

    Main: Add RecyclerView to choose each example

--- app/src/main/java/cc/jtogashi/mapboxmapsupporthelper/MainActivity.kt
+++ app/src/main/java/cc/jtogashi/mapboxmapsupporthelper/MainActivity.kt
@@ -3,18 +3,32 @@ package cc.jtogashi.mapboxmapsupporthelper
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import cc.jtogashi.mapboxmapsupporthelper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val exampleList = listOf(
+        LayerOperationsActivity::class.java,
+        GeofencingActivity::class.java
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         val binding = ActivityMainBinding.inflate(layoutInflater).apply {
-
+            listExamples.adapter = MainActivityListAdapter(exampleList)
+            listExamples.layoutManager = LinearLayoutManager(this@MainActivity)
         }
 
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
---
+++ app/src/main/java/cc/jtogashi/mapboxmapsupporthelper/MainActivityListAdapter.kt
@@ -0,0 +1,56 @@
+package cc.jtogashi.mapboxmapsupporthelper
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import cc.jtogashi.mapboxmapsupporthelper.databinding.ViewholderListExampleBinding
+
+class MainActivityListAdapter(
+    private val exampleList: List<Class<out AppCompatActivity>>
+) : RecyclerView.Adapter<MainActivityListAdapter.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return exampleList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.setActivityClass(exampleList[position])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ViewholderListExampleBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return ViewHolder(
+            binding.root,
+            parent.context
+        )
+    }
+
+    class ViewHolder(
+        private val view: View,
+        private val context: Context
+    ) : RecyclerView.ViewHolder(view) {
+        private var activityClass: Class<*>? = null
+
+        fun setActivityClass(activityClass: Class<out AppCompatActivity>) {
+            this.activityClass = activityClass
+            view.findViewById<TextView>(R.id.textViewListExample).text = activityClass.simpleName
+
+            view.setOnClickListener { v ->
+                if (this.activityClass != null) {
+                    val intent = Intent(context, this.activityClass)
+                    context.startActivity(intent)
+                }
+            }
+        }
+    }
+}
